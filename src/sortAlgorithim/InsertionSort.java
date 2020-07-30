ce74692a407dc7d996c42a604a6798736040a4f2
AdamShamaa <adamshamaa@gmail.com>
2020-07-30 18:49:58 -0400

    refactor: replace rewritten insertion sort in MergeInsertionSort class with pre-existing insertion sort class

--- src/sortAlgorithim/InsertionSort.java
+++ src/sortAlgorithim/InsertionSort.java
@@ -3,12 +3,16 @@ package sortAlgorithim;
 import GUI.sortingJPanel;
 
 public class InsertionSort {
-    public void sort(sortingJPanel sort) {
-        int arrayLength = sort.arrayLength();
-        for(int i = 0; i < arrayLength; i++) {
-            for(int j = i; j > 0 && sort.less(sort.getValue(j),sort.getValue(j-1)); j-- ) {
+	
+    public void sort(sortingJPanel sort) {	//sort entire array
+        sort(sort, 0, sort.arrayLength());
+    }
+    
+    public void sort(sortingJPanel sort, int low, int hi) {	//sort overload - sort a specific partition of the array
+    	for(int i = low; i < hi; i++) {
+            for(int j = i; j > low && sort.less(sort.getValue(j),sort.getValue(j-1)); j-- ) {
                 sort.switchVal(j, j-1);
-                sort.resetArrayColor();
+                //sort.resetArrayColor();
             }
         }
     }
