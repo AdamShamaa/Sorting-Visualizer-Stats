2be3f0be1a72b2ad6fd97694026eae1e6124d673
AdamShamaa <adamshamaa@gmail.com>
2020-12-30 17:12:55 -0500

    refactor: Create a master class to host all sorting algorithim classes. Automates the addition of any new algorithims by only requiring the addition of adding the algorithim data into the list instead of defining the new sorting algorithim in each class such as the GUI, JPanel ect.

---
+++ src/sortAlgorithim/MasterSortingFile.java
@@ -0,0 +1,52 @@
+package sortAlgorithim;
+
+public class MasterSortingFile {
+	
+	class SortAlgorithimData {
+		String algorithimName;
+		SortingAlgorithim sortAlgorithim;
+		
+		public SortAlgorithimData(String algorithimName, SortingAlgorithim sortAlgorithim) {
+			this.algorithimName = algorithimName;
+			this.sortAlgorithim = sortAlgorithim;
+		}
+	}
+	
+	 /** -------- Adding a new sorting algorithm -------- */
+	 /**
+	 * Be sure to extends the sort algorithm class to be a child of "SortingAlgorithim"
+	 * Use the following template: 
+	 * 								new SortAlgorithimData("NAME_OF_SORT_ALGORITHIM", new NAME_OF_CLASS())
+	 * Append the template to the start or end of the "ArrayOfSortingAlgorithims" array located on line 24
+	 * Done!
+	 */
+		
+	// DATA
+	public SortAlgorithimData ArrayOfSortingAlgorithims[] = {
+			new SortAlgorithimData("InsertionSort", new InsertionSort()),
+			new SortAlgorithimData("MergeInsertionSort", new MergeInsertionSort()),
+			new SortAlgorithimData("MergeSort", new MergeSort()),
+			new SortAlgorithimData("QuickSort", new QuickSort()),
+			new SortAlgorithimData("SelectionSort", new SelectionSort()),
+			new SortAlgorithimData("ShellSort", new ShellSort()),
+			new SortAlgorithimData("ThreeWayQuickSort", new ThreeWayQuickSort()),
+			new SortAlgorithimData("KeyIndexedCounting", new KeyIndexedCounting())
+	};
+	
+	public String[] getArrayOfSortingAlgorithimNames() {
+		String[] names = new String[ArrayOfSortingAlgorithims.length];
+		for (int index = 0; index < ArrayOfSortingAlgorithims.length; index++) {
+			names[index] = ArrayOfSortingAlgorithims[index].algorithimName;
+		}
+		return names;
+	}
+
+	// Initiate Sorting Algorithim
+	public SortingAlgorithim getSortingAlgorithim(String algorithimName) {
+		for (SortAlgorithimData singleSortAlgorithim: ArrayOfSortingAlgorithims) { 
+			if (singleSortAlgorithim.algorithimName == algorithimName) return singleSortAlgorithim.sortAlgorithim;
+		}
+		
+		throw new IllegalArgumentException("getClass() // INVALID ALGORITHIM NAME: " + algorithimName);
+	}
+}
---
+++ src/sortAlgorithim/SortingAlgorithim.java
@@ -0,0 +1,9 @@
+package sortAlgorithim;
+
+import GUIComponents.Array;
+
+public class SortingAlgorithim {
+	public void sort(Array array) {
+		
+	}
+}
